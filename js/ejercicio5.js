1252be325f4c2e2bbfc8b9a7b5e6ee77020164bd
mednologic <mednologic@gmail.com>
2022-06-02 09:58:13 +0200

    "All done"

---
+++ js/ejercicio5.js
@@ -0,0 +1,63 @@
+const foto1 = "<img src='../img/foto1.jpeg'>";
+        const frase1 = "<center>1. India - Agra - El Taj Majal</center>"
+
+        const foto2 = "<img src='../img/foto2.jpeg'>";
+        const frase2 = "<center>2. Terrero agrario </center>"
+
+        const foto3 = "<img src='../img/foto3.jpeg'>";
+        const frase3 = "<center>3. Athenas - El Partenón</center>"
+
+        const foto4 = "<img src='../img/foto4.jpeg'>";
+        const frase4 = "<center>4. Barco</center>"
+
+        const foto5 = "<img src='../img/foto5.jpeg'>";
+        const frase5 = "<center>3. Athenas - El Partenón</center>"
+
+        const foto6 = "<img src='../img/foto6.jpeg'>";
+        const frase6 = "<center>6. Egipto - Templo de Ramsés II</center>"
+
+        const foto7 = "<img src='../img/foto7.jpeg'>";
+        const frase7 = "<center>7. Edificio parecido al Ayuntamiento de Valladolid</center>"
+
+        const foto8 = "<img src='../img/foto8.jpeg'>";
+        const frase8 = "<center>8. Acueducto</center>"
+
+        function ampliarFoto1() {
+            document.getElementById("img").innerHTML = foto1;
+            document.getElementById("frase").innerHTML = frase1;
+        }
+
+        function ampliarFoto2() {
+            document.getElementById("img").innerHTML = foto2;
+            document.getElementById("frase").innerHTML = frase2;
+        }
+
+        function ampliarFoto3() {
+            document.getElementById("img").innerHTML = foto3;
+            document.getElementById("frase").innerHTML = frase3;
+        }
+
+        function ampliarFoto4() {
+            document.getElementById("img").innerHTML = foto4;
+            document.getElementById("frase").innerHTML = frase4;
+        }
+
+        function ampliarFoto5() {
+            document.getElementById("img").innerHTML = foto5;
+            document.getElementById("frase").innerHTML = frase5;
+        }
+
+        function ampliarFoto6() {
+            document.getElementById("img").innerHTML = foto6;
+            document.getElementById("frase").innerHTML = frase6;
+        }
+
+        function ampliarFoto7() {
+            document.getElementById("img").innerHTML = foto7;
+            document.getElementById("frase").innerHTML = frase7;
+        }
+
+        function ampliarFoto8() {
+            document.getElementById("img").innerHTML = foto8;
+            document.getElementById("frase").innerHTML = frase8;
+        }
